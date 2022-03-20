15d2db019d06408c9cb97ba6a4baf2f1fa6e3256
Yanto Lee <yanto3528@gmail.com>
2022-03-20 20:14:24 +0700

    rename header to navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -8,7 +8,7 @@ import { Wrapper, LogoContainer, DividerContainer } from './styles'
 const avatarSrc =
   'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
 
-export const Header = () => {
+export const Navbar = () => {
   return (
     <Wrapper>
       <Flex>
---
+++ src/components/navbar/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.nav`
+  padding: 1.2rem 2.4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray5};
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.1);
+`
+
+export const LogoContainer = styled.div`
+  margin-right: 9.5rem;
+`
+
+export const DividerContainer = styled.div`
+  height: 3rem;
+`
