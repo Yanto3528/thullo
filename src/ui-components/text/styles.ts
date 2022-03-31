c72cb34f0d579947a411ab2b5667002513ed7293
Yanto Lee <yanto3528@gmail.com>
2022-03-31 18:13:49 +0700

    added color props to text component

--- src/ui-components/text/styles.ts
+++ src/ui-components/text/styles.ts
@@ -6,4 +6,5 @@ export const Paragraph = styled.p<TextProps>`
   font-size: ${({ size }) => size};
   font-weight: ${({ weight }) => weight};
   font-family: ${({ family }) => family};
+  color: ${({ color, theme }) => color && theme.colors[color]};
 `
--- src/ui-components/text/types.ts
+++ src/ui-components/text/types.ts
@@ -1,5 +1,8 @@
+import { Color } from '@/types'
+
 export interface TextProps {
   size?: string
   weight?: string | number
   family?: string
+  color?: keyof Color
 }
