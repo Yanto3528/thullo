f5e4d24bff17a078f77fd4a0d116391ee2646301
Yanto Lee <yanto3528@gmail.com>
2022-03-31 20:16:47 +0700

    added gray body styles component

---
+++ src/styles/components/gray-body.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const GrayBody = styled.div`
+  min-height: 100vh;
+  background-color: ${({ theme }) => theme.colors.lightGray2};
+`
--- src/styles/components/index.ts
+++ src/styles/components/index.ts
@@ -1,3 +1,4 @@
 export * from './container'
+export * from './gray-body'
 export * from './icon-container'
 export * from './main-content'
