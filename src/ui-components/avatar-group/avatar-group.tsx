2d639c12804a50ab9aab2111bbe6635bfe901529
Yanto Lee <yanto3528@gmail.com>
2022-03-31 18:16:14 +0700

    added avatar-group component

---
+++ src/ui-components/avatar-group/avatar-group.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { Text } from '../text'
+import { Wrapper } from './styles'
+import { AvatarGroupProps } from './types'
+
+export const AvatarGroup: React.FC<AvatarGroupProps> = ({ children, maxDisplay = 3 }) => {
+  const isChildExceedMax = React.Children.count(children) > maxDisplay
+  return (
+    <Wrapper>
+      {React.Children.toArray(children)
+        .slice(0, maxDisplay)
+        .map((childElement) => {
+          if (React.isValidElement(childElement)) {
+            return React.cloneElement(childElement, {
+              ...childElement.props,
+            })
+          }
+
+          return null
+        })}
+      {isChildExceedMax && <Text color='gray4'>+ {React.Children.count(children) - maxDisplay} others</Text>}
+    </Wrapper>
+  )
+}
