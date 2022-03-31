7583c41bbb85dd5b089d6363b2607a332befcd57
Yanto Lee <yanto3528@gmail.com>
2022-03-31 18:16:39 +0700

    add maxWidth to container to control the width of the container

--- src/styles/components/container.ts
+++ src/styles/components/container.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-  width: 100%;
+interface ContainerProps {
+  maxWidth?: string
+}
+
+export const Container = styled.div<ContainerProps>`
+  max-width: ${({ maxWidth }) => maxWidth || '100%'};
   padding: 0 2.4rem;
   margin: 0 auto;
 `
