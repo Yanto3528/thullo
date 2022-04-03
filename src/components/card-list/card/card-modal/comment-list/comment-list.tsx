6c12e5d2ea218ede432774f56a6498f3fd8b0550
Yanto Lee <yanto3528@gmail.com>
2022-04-03 20:14:50 +0700

    added comment list component

--- src/components/card-list/card/card-modal/comment-list/comment-list.tsx
+++ src/components/card-list/card/card-modal/comment-list/comment-list.tsx
@@ -1,5 +1,41 @@
+import React, { useState } from 'react'
+
+import { Divider } from '@/ui-components'
+import { CommentType } from '@/types'
+import { generateId } from '@/utils'
+
 import { Wrapper } from './styles'
+import { CommentForm } from './comment-form'
+import { Comment } from './comment'
 
 export const CommentList = () => {
-  return <Wrapper>Hello</Wrapper>
+  const [comments, setComments] = useState<CommentType[]>([])
+
+  const onAddNewComment = (data: { content: string }) => {
+    const newComment = {
+      id: generateId(),
+      author: {
+        name: 'Yanto Lee',
+        avatar: '',
+      },
+      createdAt: new Date(),
+      content: data.content,
+    }
+    setComments((currentComments) => [...currentComments, newComment])
+  }
+
+  return (
+    <Wrapper>
+      <CommentForm onAddNewComment={onAddNewComment} />
+      {comments.map((comment, commentIndex) => {
+        const isLastElement = commentIndex === comments.length - 1
+        return (
+          <>
+            <Comment key={comment.id} comment={comment} />
+            {!isLastElement && <Divider spacing='2rem' />}
+          </>
+        )
+      })}
+    </Wrapper>
+  )
 }
