f1c283a8ce1022a394d9cdbf4169e39e00f5ed22
Yanto Lee <yanto3528@gmail.com>
2022-04-03 11:08:03 +0700

    added calculateTextareaRows utils function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -12,3 +12,27 @@ export const getMediaQuery = (screenSize: number) => {
 export const generateId = () => {
   return (Math.random() * 100 + new Date().getTime()).toString()
 }
+
+export const calculateTextareaRows = (
+  element: HTMLTextAreaElement,
+  padding = 20,
+  minRows = 1,
+  maxRows = 5,
+  lineHeight = 15
+) => {
+  const previousRows = element.rows
+  element.rows = minRows // reset number of rows in textarea
+  const textareaScrollHeight = element.scrollHeight - padding
+
+  const currentRows = ~~(textareaScrollHeight / lineHeight) // eslint-disable-line
+
+  if (currentRows === previousRows) {
+    element.rows = currentRows
+  }
+
+  if (currentRows >= maxRows) {
+    element.rows = maxRows
+    element.scrollTop = element.scrollHeight
+  }
+  return currentRows < maxRows ? currentRows : maxRows
+}
