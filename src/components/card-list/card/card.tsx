53c60451f653bb8c54d7545588f5029688b3a2c6
Yanto Lee <yanto3528@gmail.com>
2022-04-04 14:07:30 +0700

    fix card modal not closing when clicking on overlay

--- src/components/card-list/card/card.tsx
+++ src/components/card-list/card/card.tsx
@@ -11,26 +11,33 @@ import { CardModal } from './card-modal'
 export const Card = ({ card, index }: CardProps) => {
   const { id, title } = card
 
-  const [showModal, { onToggle }] = useToggle()
+  const [showModal, { onClose, onToggle }] = useToggle()
 
   return (
-    <Draggable draggableId={id} index={index}>
-      {(provided) => (
-        <Wrapper {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} onClick={onToggle}>
-          <Heading as='h4' family='Noto Sans' size='1.6rem'>
-            {title}
-          </Heading>
-          <Flex gap='1rem' wrap='wrap' margin='1.2rem 0'>
-            <Badge>Concept</Badge>
-          </Flex>
-          <Flex gap='0.8rem'>
-            <Button width='2.8rem' padding='0.8rem'>
-              <Plus size='1.2rem' />
-            </Button>
-          </Flex>
-          <CardModal isOpen={showModal} onClose={onToggle} />
-        </Wrapper>
-      )}
-    </Draggable>
+    <>
+      <Draggable draggableId={id} index={index}>
+        {(provided) => (
+          <Wrapper
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+            onClick={onToggle}
+          >
+            <Heading as='h4' family='Noto Sans' size='1.6rem'>
+              {title}
+            </Heading>
+            <Flex gap='1rem' wrap='wrap' margin='1.2rem 0'>
+              <Badge>Concept</Badge>
+            </Flex>
+            <Flex gap='0.8rem'>
+              <Button width='2.8rem' padding='0.8rem'>
+                <Plus size='1.2rem' />
+              </Button>
+            </Flex>
+          </Wrapper>
+        )}
+      </Draggable>
+      <CardModal isOpen={showModal} onClose={onClose} />
+    </>
   )
 }
