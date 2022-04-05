3b2fde18c4c652b11cbb094aa5e93ab132382e08
Yanto Lee <yanto3528@gmail.com>
2022-04-05 13:30:39 +0700

    added 400 and 500 font weight to noto sans font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,7 +33,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
-          <link href='https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap' rel='stylesheet' />
+          <link href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500&display=swap' rel='stylesheet' />
           <link
             href='https://fonts.googleapis.com/css2?family=Noto+Sans&family=Poppins:wght@400;500;600&display=swap'
             rel='stylesheet'
