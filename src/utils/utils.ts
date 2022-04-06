f33590dd9f9a5a150896c3bebbb98fd09a07097e
Yanto Lee <yanto3528@gmail.com>
2022-04-06 19:37:50 +0700

    added validateEmail utils function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -54,3 +54,9 @@ export const mergeDeep = (sourceObj: Record<string, unknown>, ...otherObjs: Reco
 
   return sourceObj
 }
+
+export const validateEmail = (email: string) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  )
+}
