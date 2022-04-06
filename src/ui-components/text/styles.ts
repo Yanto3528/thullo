8d2f532c8618fc19cb2e2798f7b24cc1957dada7
Yanto Lee <yanto3528@gmail.com>
2022-04-06 19:39:25 +0700

    added align and spacing styles to text component

--- src/ui-components/text/styles.ts
+++ src/ui-components/text/styles.ts
@@ -1,12 +1,17 @@
 import styled from 'styled-components'
 
+import { spacingStyles } from '@/styles/shared'
+
 import { TextProps } from './types'
 
 export const Paragraph = styled.p<TextProps>`
   font-size: ${({ size }) => size};
   font-weight: ${({ weight }) => weight};
   font-family: ${({ family }) => family};
+  text-align: ${({ align }) => align};
   color: ${({ color, theme }) => color && theme.colors[color]};
   line-height: ${({ lineHeight }) => lineHeight};
   white-space: pre-wrap;
+
+  ${spacingStyles};
 `
--- src/ui-components/text/types.ts
+++ src/ui-components/text/types.ts
@@ -1,10 +1,13 @@
-import { Color } from '@/types'
 import React from 'react'
 
-export interface TextProps {
+import { Color } from '@/types'
+import { Spacing } from '@/styles/shared'
+
+export interface TextProps extends Spacing {
   size?: React.CSSProperties['fontSize']
   weight?: React.CSSProperties['fontWeight']
   family?: React.CSSProperties['fontFamily']
   lineHeight?: React.CSSProperties['lineHeight']
+  align?: React.CSSProperties['textAlign']
   color?: keyof Color
 }
