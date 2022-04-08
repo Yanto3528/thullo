154a5eaaf751e1315cd9b13283df159e3fa1b147
Yanto Lee <yanto3528@gmail.com>
2022-04-08 16:44:03 +0700

    added validation for signupUser mutation

---
+++ src/lib/graphql/validator/user.ts
@@ -0,0 +1,7 @@
+import joi from 'joi'
+
+export const signupValidationSchema = joi.object({
+  name: joi.string().required(),
+  email: joi.string().email().required(),
+  password: joi.string().min(6).max(20).required(),
+})
