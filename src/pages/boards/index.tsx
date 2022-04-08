b8dbd6bf670ccae76be20f69c444e2d5e5d3f4c4
Yanto Lee <yanto3528@gmail.com>
2022-04-08 19:15:09 +0700

    test calling useGetUserQuery

--- src/pages/boards/index.tsx
+++ src/pages/boards/index.tsx
@@ -1,7 +1,12 @@
 import { Navbar, BoardList } from '@/components'
 import { Container, GrayBody } from '@/styles/components'
+import { useGetUserQuery } from '@/lib/react-query/query'
 
 const BoardsPage = () => {
+  const { data } = useGetUserQuery()
+
+  // eslint-disable-next-line
+  console.log('data: ', data)
   return (
     <GrayBody>
       <Navbar />
