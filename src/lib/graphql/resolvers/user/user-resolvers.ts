8e4c1639698dcebbece5ccf8fbfbd24b7ce93ad5
Yanto Lee <yanto3528@gmail.com>
2022-04-09 20:32:58 +0700

    refactor resolver/user/index.ts to resolver/user/user-resolvers.ts

---
+++ src/lib/graphql/resolvers/user/user-resolvers.ts
@@ -0,0 +1,112 @@
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import { ApolloError } from 'apollo-server-micro'
+
+import { prisma } from '@/lib'
+import { signupValidationSchema, loginValidationSchema } from '@/lib/graphql/validator'
+
+import { Context } from '../../types'
+import { SignupUserArgs, LoginUserArgs } from './types'
+
+const COOKIE_MAX_AGE = 7 * 24 * 60 * 60 * 1000 // 7 days
+
+export const userResolvers = {
+  Query: {
+    getCurrentUser: async (_parent: unknown, _args: unknown, ctx: Context) => {
+      if (!ctx.user) {
+        throw new ApolloError('Forbidden to access this resource')
+      }
+
+      const user = await prisma.user.findFirst({
+        where: {
+          id: ctx.user.id,
+        },
+      })
+
+      if (!user) {
+        throw new ApolloError('User does not exists.')
+      }
+
+      return user
+    },
+  },
+  Mutation: {
+    signupUser: async (_parent: unknown, args: SignupUserArgs, ctx: Context) => {
+      const validationResult = signupValidationSchema.validate(args)
+      if (validationResult.error) {
+        throw new ApolloError(validationResult.error.message)
+      }
+
+      const { name, email, password } = args
+
+      const existingUser = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+      if (existingUser) {
+        throw new ApolloError('User already exists.')
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10)
+
+      const newUser = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      })
+
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET!, {
+        expiresIn: process.env.JWT_EXPIRES,
+      })
+
+      ctx.cookie('token', token, {
+        path: '/',
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: COOKIE_MAX_AGE,
+      })
+
+      return newUser
+    },
+    loginUser: async (_parent: unknown, args: LoginUserArgs, ctx: Context) => {
+      const validationResult = loginValidationSchema.validate(args)
+      if (validationResult.error) {
+        throw new ApolloError(validationResult.error.message)
+      }
+
+      const { email, password } = args
+
+      const user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+      if (!user) {
+        throw new ApolloError('Invalid credentials')
+      }
+
+      const isPasswordMatch = await bcrypt.compare(password, user.password)
+      if (!isPasswordMatch) {
+        throw new ApolloError('Invalid credentials')
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, {
+        expiresIn: process.env.JWT_EXPIRES,
+      })
+
+      ctx.cookie('token', token, {
+        path: '/',
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: COOKIE_MAX_AGE,
+      })
+
+      return user
+    },
+  },
+}
