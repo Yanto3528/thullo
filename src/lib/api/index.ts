e86bdb9aa5f47f91ab36a25a47b7784eca856ee3
Yanto Lee <yanto3528@gmail.com>
2022-04-20 18:22:52 +0700

    delete disable eslint for api

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -2,7 +2,6 @@ import { GraphQLClient } from 'graphql-request'
 
 import { getSdk } from '@/generated-api'
 
-// eslint-disable-next-line
 const client = new GraphQLClient(process.env.NEXT_PUBLIC_ENDPOINT!)
 
 const allApis = getSdk(client)
